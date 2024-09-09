59833441e1bd2fd443683a70d15478ffd419d65b
DigishaKaneria <53176628+DigishaKaneria@users.noreply.github.com>
2024-09-09 21:24:01 +0530

    Update ComponentDetail.js

--- src/components/ComponentDetail.js
+++ src/components/ComponentDetail.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Typography, Paper, Tabs, Tab } from "@mui/material";
+import { Typography, Paper, Tabs, Tab, Grid2, Box } from "@mui/material";
 import ReactFlow, { Background } from "react-flow-renderer";
 import "./ComponentDetail.css";
-import Box from "@mui/material/Box";
 import NavBar from "./NavBar";
 
 const ComponentDetail = () => {
@@ -130,109 +129,129 @@ const ComponentDetail = () => {
 
   return (
     <>
-      <NavBar />
-      <div className="header">
-        <div>
-          <span className="component-type">Component — {spec.type}</span>
-          <h1 className="component-name">{metadata.name}</h1>
-        </div>
-        <div className="metadata">
-          <span className="owner">
-            Owner: <span className="team-a">{spec.owner}</span>
-          </span>
-          <span
-            className="lifecycle"
-            style={{ marginLeft: "20px", marginTop: "20px" }}
-          >
-            Lifecycle: <span className="status">{spec.lifecycle}</span>
-          </span>
-        </div>
+    <NavBar />
+    <div className="header">
+      <div>
+        <span className="component-type">Component — {spec.type}</span>
+        <h1 className="component-name">{metadata.name}</h1>
       </div>
-      <Box sx={{ width: "100%" }} className="navigationBar">
-        <Box sx={{ borderBottom: 1, borderColor: "divider",width: "100%" }}>
-          <Tabs value={activeTab} onChange={handleTabChange}>
-            <Tab label="Overview" />
-            <Tab label="CI/CD" />
-            <Tab label="API" />
-            <Tab label="DEPENDENCIES" />
-            <Tab label="DOCS" />
-            <Tab label="TODOS" />
-          </Tabs>
-        </Box>
+      <div className="metadata">
+        <span className="owner">
+          Owner: <span className="team-a">{spec.owner}</span>
+        </span>
+        <span
+          className="lifecycle"
+          style={{ marginLeft: "20px", marginTop: "20px" }}
+        >
+          Lifecycle: <span className="status">{spec.lifecycle}</span>
+        </span>
+      </div>
+    </div>
+    <Box sx={{ width: "100%" }} className="navigationBar">
+      <Box sx={{ borderBottom: 1, borderColor: "divider",width: "100%" }}>
+        <Tabs value={activeTab} onChange={handleTabChange}>
+          <Tab label="Overview" />
+          <Tab label="CI/CD" />
+          <Tab label="API" />
+          <Tab label="DEPENDENCIES" />
+          <Tab label="DOCS" />
+          <Tab label="TODOS" />
+        </Tabs>
       </Box>
+    </Box>
 
-      <div className="component-detail-container" >
-        <div className="component-info">
-          <Typography variant="h5" component="h2" gutterBottom>
-            About
-          </Typography>
-          <Typography variant="h5" component="h1" gutterBottom>
-            {metadata.name}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            <strong>Description:</strong> {metadata.description}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            <strong>Type:</strong> {spec.type}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            <strong>Lifecycle:</strong> {spec.lifecycle}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            <strong>Owner:</strong> {spec.owner}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            <strong>System:</strong> {spec.system}
-          </Typography>
+    <div className="component-detail-container" >
+      <div className="component-info">
+      <Typography variant="h5" component="div" gutterBottom>
+          Desription
+        </Typography>
+        <Typography variant="h6" color="textSecondary">
+          {metadata.description}
+        </Typography>
 
-          <Typography variant="h6" gutterBottom>
-            Tags:
-          </Typography>
-          {metadata.tags.map((tag, index) => (
-            <span
-              style={{
-                border: "1px solid #460074",
-                borderRadius: "20px",
-                padding: "5px 16px",
-                marginRight: "3px",
-                color: "#460074",
-              }}
-            >
-              {tag}
-            </span>
-          ))}
+        <Grid2 container spacing={12} style={{ marginTop: 10 }}>
+          <Grid2 item xs={6}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                Owner
+              </Typography>
+              <Typography variant="body2">{spec.owner}</Typography>
+            </Box>
+          </Grid2>
 
-          <Typography variant="h6" gutterBottom>
-            Links:
-          </Typography>
-          {metadata.links.map((link, index) => (
-            <div key={index}>
-              <a
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#460074" }}
-              >
-                {link.title} (Icon: {link.icon})
-              </a>
-            </div>
-          ))}
-        </div>
+          <Grid2 item xs={6}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                Type
+              </Typography>
+              <Typography variant="body2">{spec.type}</Typography>
+            </Box>
+          </Grid2>
 
-        <div className="relations-container" style={{ width: "80%" }}>
-          <Paper elevation={3} className="relations-paper">
-            <Typography variant="h5" component="h2" gutterBottom>
-              Relations
-            </Typography>
-            <div className="graph-container">
-              <ReactFlow nodes={nodes} edges={edges} fitView>
-                <Background />
-              </ReactFlow>
-            </div>
-          </Paper>
-        </div>
+          <Grid2 item xs={6}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                Lifecycle
+              </Typography>
+              <Typography variant="body2">{spec.lifecycle}</Typography>
+            </Box>
+          </Grid2>
+
+          <Grid2 item xs={6}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                System
+              </Typography>
+              <Typography variant="body2">{spec.system}</Typography>
+            </Box>
+          </Grid2>
+
+          <Grid2 item xs={12}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                Tags
+              </Typography>
+              <Typography variant="body2">
+                {metadata.tags.map((tag, index) => (
+                  <span style={{ border: '1px solid #460074', borderRadius: '20px', padding: '5px 16px', marginRight: '3px', color: '#460074' }}>{tag}</span>
+                ))}
+              </Typography>
+            </Box>
+          </Grid2>
+
+          <Grid2 item xs={12}>
+            <Box>
+              <Typography variant="subtitle2" color="textSecondary">
+                Links
+              </Typography>
+              <Typography variant="body2">
+                {metadata.links.map((link, index) => (
+                  <div key={index}>
+                    <a href={link.url} target="_blank" rel="noopener noreferrer" style={{ color: '#460074' }}>
+                      {link.title} (Icon: {link.icon})
+                    </a>
+                  </div>
+                ))}
+              </Typography>
+            </Box>
+          </Grid2>
+        </Grid2>
       </div>
-    </>
+
+      <div className="relations-container" style={{ width: "80%" }}>
+        <Paper elevation={3} className="relations-paper">
+          <Typography variant="h5" component="h2" gutterBottom>
+            Relations
+          </Typography>
+          <div className="graph-container">
+            <ReactFlow nodes={nodes} edges={edges} fitView>
+              <Background />
+            </ReactFlow>
+          </div>
+        </Paper>
+      </div>
+    </div>
+  </>
   );
 };
 
